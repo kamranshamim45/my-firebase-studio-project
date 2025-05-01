36bfc27790cd26caf0a4ada809c8a6a6a2b3329e
kamran Shamim <kamranshamim45@gmail.com>
2025-05-01 18:14:51 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Console Error: In HTML, whitespace text nodes cannot be a child of <html>. Make sure you don't have any extra whitespace between tags on each line of your source code.
    > This will cause a hydration error.. Error source: See more info here: https://nextjs.org/docs/messages/react-hydration-error
    >
    > ...
    >     <HotReload assetPrefix="" globalError={[...]}>
    >       <AppDevOverlay state={{nextId:1, ...}} globalError={[...]}>
    >         <AppDevOverlayErrorBoundary globalError={[...]} onError={function bound dispatchSetState}>
    >           <ReplaySsrOnlyErrors>
    >           <DevRootHTTPAccessFallbackBoundary>
    >             <HTTPAccessFallbackBoundary notFound={<NotAllowedRootHTTPFallbackError>}>
    >               <HTTPAccessFallbackErrorBoundary pathname="/" notFound={<NotAllowedRootHTTPFallbackError>} ...>
    >                 <RedirectBoundary>
    >                   <RedirectErrorBoundary router={{...}}>
    >                     <Head>
    >                     <link>
    >                     <script>
    >                     <script>
    >                     <script>
    >                     <RootLayout>
    > >                     <html lang="en" className="dark">
    > >                       {" "}
    >                         ...
    >                     ...
    >         ...
    >
    >
    > src/app/layout.tsx (19:5) @ RootLayout
    >
    >   17 | }>) {
    >   18 |   return (
    > > 19 |     <html lang="en" className="dark"> {/* Apply dark theme globally */}
    >      |     ^
    >   20 |       <body className={`${inter.variable} font-sans antialiased`}>
    >   21 |         {children}
    >   22 |         <Toaster /> {/* Add Toaster here */}
    >
    > Call Stack
    > 14
    >
    > Show 12 ignore-listed frame(s)
    > html
    > <anonymous> (0:0)
    > RootLayout
    > src/app/layout.tsx (19:5)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark"> {/* Apply dark theme globally */}
+    <html lang="en" className="dark">{/* Apply dark theme globally */}
       <body className={`${inter.variable} font-sans antialiased`}>
         {children}
         <Toaster /> {/* Add Toaster here */}
